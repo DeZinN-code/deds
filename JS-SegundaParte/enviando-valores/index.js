f5199e83f9607ed85394f5c36300e87ba3ceace5
DeZinN-code <andreluizdosreis13@gmail.com>
2025-11-26 15:58:01 -0300

    DOM

---
+++ JS-SegundaParte/enviando-valores/index.js
@@ -0,0 +1,7 @@
+function register(element) {
+    const username = element.children.username.value;
+    const password = element.children.password.value;
+    const confirmPassword = element.children.confirmPassword.value;
+
+    console.log({username, password, confirmPassword});
+}
